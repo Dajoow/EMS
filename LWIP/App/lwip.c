8c91e70e0f3ebb916a949bc8b141f63cb60de0b1
wirano <git@wirano.me>
2023-07-06 21:51:23 +0800

    feat: enable DHCP client & add hotplug

--- LWIP/App/lwip.c
+++ LWIP/App/lwip.c
@@ -29,6 +29,7 @@
 
 /* USER CODE BEGIN 0 */
 #include "at24cxx.h"
+#include "usart.h"
 /* USER CODE END 0 */
 /* Private function prototypes -----------------------------------------------*/
 static void ethernet_link_status_updated(struct netif *netif);
@@ -44,9 +45,6 @@ struct netif gnetif;
 ip4_addr_t ipaddr;
 ip4_addr_t netmask;
 ip4_addr_t gw;
-uint8_t IP_ADDRESS[4];
-uint8_t NETMASK_ADDRESS[4];
-uint8_t GATEWAY_ADDRESS[4];
 
 /* USER CODE BEGIN 2 */
 
@@ -57,42 +55,13 @@ uint8_t GATEWAY_ADDRESS[4];
   */
 void MX_LWIP_Init(void)
 {
-  /* IP addresses initialization */
-  IP_ADDRESS[0] = 10;
-  IP_ADDRESS[1] = 2;
-  IP_ADDRESS[2] = 133;
-  IP_ADDRESS[3] = 33;
-  NETMASK_ADDRESS[0] = 255;
-  NETMASK_ADDRESS[1] = 255;
-  NETMASK_ADDRESS[2] = 255;
-  NETMASK_ADDRESS[3] = 0;
-  GATEWAY_ADDRESS[0] = 10;
-  GATEWAY_ADDRESS[1] = 2;
-  GATEWAY_ADDRESS[2] = 133;
-  GATEWAY_ADDRESS[3] = 254;
-
-/* USER CODE BEGIN IP_ADDRESSES */
-  IP_ADDRESS[0] = bsmuSetting.IP_ADD[0];
-  IP_ADDRESS[1] = bsmuSetting.IP_ADD[1];
-  IP_ADDRESS[2] = bsmuSetting.IP_ADD[2];
-  IP_ADDRESS[3] = bsmuSetting.IP_ADD[3];
-  NETMASK_ADDRESS[0] = bsmuSetting.NETMASK[0];
-  NETMASK_ADDRESS[1] = bsmuSetting.NETMASK[1];
-  NETMASK_ADDRESS[2] = bsmuSetting.NETMASK[2];
-  NETMASK_ADDRESS[3] = bsmuSetting.NETMASK[3];
-  GATEWAY_ADDRESS[0] = bsmuSetting.GATEWAY[0];
-  GATEWAY_ADDRESS[1] = bsmuSetting.GATEWAY[1];
-  GATEWAY_ADDRESS[2] = bsmuSetting.GATEWAY[2];
-  GATEWAY_ADDRESS[3] = bsmuSetting.GATEWAY[3];
-/* USER CODE END IP_ADDRESSES */
-
   /* Initilialize the LwIP stack with RTOS */
   tcpip_init( NULL, NULL );
 
-  /* IP addresses initialization without DHCP (IPv4) */
-  IP4_ADDR(&ipaddr, IP_ADDRESS[0], IP_ADDRESS[1], IP_ADDRESS[2], IP_ADDRESS[3]);
-  IP4_ADDR(&netmask, NETMASK_ADDRESS[0], NETMASK_ADDRESS[1] , NETMASK_ADDRESS[2], NETMASK_ADDRESS[3]);
-  IP4_ADDR(&gw, GATEWAY_ADDRESS[0], GATEWAY_ADDRESS[1], GATEWAY_ADDRESS[2], GATEWAY_ADDRESS[3]);
+  /* IP addresses initialization with DHCP (IPv4) */
+  ipaddr.addr = 0;
+  netmask.addr = 0;
+  gw.addr = 0;
 
   /* add the network interface (IPv4/IPv6) with RTOS */
   netif_add(&gnetif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &tcpip_input);
@@ -120,6 +89,9 @@ void MX_LWIP_Init(void)
   osThreadCreate (osThread(EthLink), &gnetif);
 /* USER CODE END H7_OS_THREAD_DEF_CREATE_CMSIS_RTOS_V1 */
 
+  /* Start DHCP negotiation for a network interface (IPv4) */
+  dhcp_start(&gnetif);
+
 /* USER CODE BEGIN 3 */
 
 /* USER CODE END 3 */
@@ -142,11 +114,22 @@ static void ethernet_link_status_updated(struct netif *netif)
   if (netif_is_up(netif))
   {
 /* USER CODE BEGIN 5 */
+    int err = 0;
+    Debug_printf("netif is up\r\n");
+    Debug_printf("starting dhcp...\n");
+    err = dhcp_start(&gnetif);
+    
+    if (err == ERR_OK) {
+      Debug_printf("starting dhcp success!\n");
+    } else {
+      Debug_printf("starting dhcp fail!\n");
+    }
 /* USER CODE END 5 */
   }
   else /* netif is down */
   {
 /* USER CODE BEGIN 6 */
+    Debug_printf("netif is down\r\n");
 /* USER CODE END 6 */
   }
 }
--- LWIP/Target/lwipopts.h
+++ LWIP/Target/lwipopts.h
@@ -47,6 +47,8 @@
 
 /* LwIP Stack Parameters (modified compared to initialization value in opt.h) -*/
 /* Parameters set in STM32CubeMX LwIP Configuration GUI -*/
+/*----- Value in opt.h for LWIP_DHCP: 0 -----*/
+#define LWIP_DHCP 1
 /*----- Default value in ETH configuration GUI in CubeMx: 1524 -----*/
 #define ETH_RX_BUFFER_SIZE 1536
 /*----- Value in opt.h for MEM_ALIGNMENT: 1 -----*/
@@ -55,6 +57,8 @@
 #define MEM_SIZE 12288
 /*----- Default Value for H7 devices: 0x30044000 -----*/
 #define LWIP_RAM_HEAP_POINTER 0x30044C00
+/*----- Value in opt.h for MEMP_NUM_SYS_TIMEOUT: (LWIP_TCP + IP_REASSEMBLY + LWIP_ARP + (2*LWIP_DHCP) + LWIP_AUTOIP + LWIP_IGMP + LWIP_DNS + (PPP_SUPPORT*6*MEMP_NUM_PPP_PCB) + (LWIP_IPV6 ? (1 + LWIP_IPV6_REASS + LWIP_IPV6_MLD) : 0)) -*/
+#define MEMP_NUM_SYS_TIMEOUT 5
 /*----- Value supported for H7 devices: 1 -----*/
 #define LWIP_SUPPORT_CUSTOM_PBUF 1
 /*----- Default Value for PBUF_POOL_SIZE: 16 ---*/
@@ -113,6 +117,10 @@
 #define CHECKSUM_CHECK_TCP 0
 /*----- Value in opt.h for CHECKSUM_CHECK_ICMP6: 1 -----*/
 #define CHECKSUM_CHECK_ICMP6 0
+/*----- Default Value for LWIP_DBG_MIN_LEVEL: LWIP_DBG_LEVEL_ALL ---*/
+#define LWIP_DBG_MIN_LEVEL LWIP_DBG_MASK_LEVEL
+/*----- Default Value for LWIP_DBG_TYPES_ON: LWIP_DBG_ON ---*/
+#define LWIP_DBG_TYPES_ON LWIP_DBG_OFF
 /*-----------------------------------------------------------------------------*/
 /* USER CODE BEGIN 1 */
 
