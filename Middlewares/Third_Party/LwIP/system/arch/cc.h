b98b72aae2edd1945ed1189feef7d1abb8340152
wirano <git@wirano.me>
2023-07-08 17:18:04 +0800

    Revert "fix: use Debug_printf to log debug info"
    This reverts commit ee17b4b7f804187ab5735722aef421569b6a9d12.
    
    Debug printf implement in lwipopts.h now

--- Middlewares/Third_Party/LwIP/system/arch/cc.h
+++ Middlewares/Third_Party/LwIP/system/arch/cc.h
@@ -35,7 +35,6 @@
 #include "cpu.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "usart.h"
 
 typedef int sys_prot_t;
 
@@ -80,7 +79,7 @@ typedef int sys_prot_t;
 
 #endif
 
-#define LWIP_PLATFORM_ASSERT(x) do {Debug_printf("Assertion \"%s\" failed at line %d in %s\n", \
+#define LWIP_PLATFORM_ASSERT(x) do {printf("Assertion \"%s\" failed at line %d in %s\n", \
                                      x, __LINE__, __FILE__); } while(0)
 
 /* Define random number generator function */
