d18ebc287b1d1762c7669df39210d7c5b6c14152
Jix <1091216194@qq.com>
2023-09-16 18:12:29 +0800

    update TouchGFX

--- TouchGFX/generated/fonts/src/Kerning_times_36_8bpp.cpp
+++
@@ -1,8 +0,0 @@
-#include <touchgfx/Font.hpp>
-
-FONT_KERNING_LOCATION_FLASH_PRAGMA
-KEEP extern const touchgfx::KerningNode kerning_times_36_8bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
-    { 0x0050, -1 },  // (First char = [0x0050, P], Second char = [0x0020,  ], Kerning dist = -1)
-    { 0x0050, -4 },  // (First char = [0x0050, P], Second char = [0x002E, .], Kerning dist = -4)
-    { 0x0031, -1 },  // (First char = [0x0031, 1], Second char = [0x0031, 1], Kerning dist = -1)
-};
