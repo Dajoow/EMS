946ed4d408fe1f25b08d91cba01600dbfe67516c
wirano <git@wirano.me>
2024-02-28 11:20:27 +0800

    fix cluster cap

--- Core/Inc/station_ctl.h
+++ Core/Inc/station_ctl.h
@@ -24,7 +24,7 @@
 #define  GRP_BAT_num              12												//一组中电池数
 #define  TOTOL_BAT_num            (GRP_num*GRP_BAT_num)     //总电池数，为了内存对齐这个值为2的倍数
 #define  CELL_CAP_AH              500
-#define  CLU_CAP_KWH              (CELL_CAP_AH * TOTOL_BAT_num * 2 * 3600 / 1000)                        // 每簇容量 kWh
+#define  CLU_CAP_KWH              (CELL_CAP_AH * 2 * 3600 / 1000)                        // 每簇容量 kWh
 
 #define  MAX_ERROR                400
 
