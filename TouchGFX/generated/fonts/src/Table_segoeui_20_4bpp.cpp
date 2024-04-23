011fc0f02a7982f133329417c64911b41f5ab51a
Jix <1091216194@qq.com>
2024-04-23 13:12:57 +0800

    根据最新的文档增加了错误数据类型

--- TouchGFX/generated/fonts/src/Table_segoeui_20_4bpp.cpp
+++ TouchGFX/generated/fonts/src/Table_segoeui_20_4bpp.cpp
@@ -38,10 +38,12 @@ KEEP extern const touchgfx::GlyphNode glyphs_segoeui_20_4bpp[] FONT_TABLE_LOCATI
     {  2160, 0x0059,  11,  14,  14,   0,  11,  30,   1, 0x00 },
     {  2244, 0x005C,   9,  16,  14,  -1,   8,   0,   0, 0x00 },
     {  2324, 0x005F,   9,   2, 254,   0,   8,   0,   0, 0x60 },
-    {  2334, 0x0065,   9,  10,  10,   1,  10,  31,   4, 0x00 },
-    {  2384, 0x0074,   7,  13,  13,   0,   7,  35,   1, 0x00 },
-    {  2436, 0x0077,  15,  10,  10,   0,  14,  36,   1, 0x00 },
-    {  2516, 0x0078,   9,  10,  10,   0,   9,  37,   1, 0x00 }
+    {  2334, 0x0062,  10,  14,  14,   1,  12,   0,   0, 0x00 },
+    {  2404, 0x0065,   9,  10,  10,   1,  10,  31,   4, 0x00 },
+    {  2454, 0x0069,   3,  14,  14,   1,   5,   0,   0, 0x00 },
+    {  2482, 0x0074,   7,  13,  13,   0,   7,  35,   1, 0x00 },
+    {  2534, 0x0077,  15,  10,  10,   0,  14,  36,   1, 0x00 },
+    {  2614, 0x0078,   9,  10,  10,   0,   9,  37,   1, 0x00 }
 };
 
 // segoeui_20_4bpp
@@ -215,6 +217,6 @@ touchgfx::GeneratedFont& getFont_segoeui_20_4bpp();
 
 touchgfx::GeneratedFont& getFont_segoeui_20_4bpp()
 {
-    static touchgfx::GeneratedFont segoeui_20_4bpp(glyphs_segoeui_20_4bpp, 38, 24, 20, 0, 0, 4, 1, 1, 1, unicodes_segoeui_20_4bpp, kerning_segoeui_20_4bpp, 63, 0, 0, &segoeui_contextualFormsTable);
+    static touchgfx::GeneratedFont segoeui_20_4bpp(glyphs_segoeui_20_4bpp, 40, 24, 20, 0, 0, 4, 1, 1, 1, unicodes_segoeui_20_4bpp, kerning_segoeui_20_4bpp, 63, 0, 0, &segoeui_contextualFormsTable);
     return segoeui_20_4bpp;
 }
